a772d76394f6c6ef13d50fa1ac08bcfe49859141
Alexis Ivan <alexis.ivan.mt@gmail.com>
2022-03-02 00:54:45 -0600

    Añadir pokedex.json y Modificacion index

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const { pokemon }= require('./pokedex.json');
 /*
 Verbos http
 GET
@@ -11,9 +12,37 @@ DELETE
 
 app.get("/", (req, res, next) => {
     res.status(200);
-    res.send("Bienvenido"); 
+    res.send("Bienvenido al Pokemon"); 
+});
+//Para extraer solo los datos necesarios
+app.get("/:pokemon/all",(req, res, next) => {
+    res.status(200);
+    res.send(pokemon);
+});
+//Para obtener un pokemon por id 
+app.get("/pokemon/:id([0-9]{1,3})",(req, res, next) => {
+    const id= req.params.id - 1;
+    if(id >= 0 && id <= 150){
+        res.status(200);
+        res.send(pokemon[req.params.id-1]);
+    }else{
+        res.status(404);
+        res.send("Pokémon no encontrado");
+    }
+    
 });
 
-app.listen(3000, ()=>{
+app.get('/pokemon/:name', (req, res,next) => {
+    const name = req.params.name;
+    for(i=0;i<pokemon.length;i++){
+        if(pokemon[i].name == name){
+            res.status(200);
+            res.send(pokemon[i])
+        }
+    }
+
+});
+
+app.listen(process.env.PORT || 3000, ()=>{
     console.log("Server is runnig...")
 });
