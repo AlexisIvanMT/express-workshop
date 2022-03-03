446444c0943be64a18895b45233e170f64081e24
Alexis Ivan <alexis.ivan.mt@gmail.com>
2022-03-02 22:52:13 -0600

    Cambio index operadores ternarios

--- index.js
+++ index.js
@@ -3,43 +3,51 @@ const app = express();
 const { pokemon }= require('./pokedex.json');
 /*
 Verbos http
-GET
-POST
-PATCH
-PUT 
-DELETE
+GET - obtener recursos
+POST - Almacenar recursos
+PATCH - Modificar un recurso completo
+PUT - Modificar una part de un recurso
+DELETE -Borrar un recursos
 */
 
 app.get("/", (req, res, next) => {
-    res.status(200);
-    res.send("Bienvenido al Pokemon"); 
-});
-//Para extraer solo los datos necesarios
-app.get("/:pokemon/all",(req, res, next) => {
-    res.status(200);
-    res.send(pokemon);
-});
-//Para obtener un pokemon por id 
-app.get("/pokemon/:id([0-9]{1,3})",(req, res, next) => {
-    const id= req.params.id - 1;
-    if(id >= 0 && id <= 150){
-        res.status(200);
-        res.send(pokemon[req.params.id-1]);
-    }else{
-        res.status(404);
-        res.send("Pokémon no encontrado");
-    }
-    
+   return res.status(200).send("Bienvenido al Pokemon"); 
 });
 
-app.get('/pokemon/:name', (req, res,next) => {
+//Para extraer solo los datos necesarios
+app.get('/:pokemon',(req, res, next) => {
+    return res.status(200).send(pokemon);
+});
+
+//Para obtener un pokemon por id 
+app.get('/pokemon/:id([0-9]{1,3})',(req, res, next) => {
+    const id = req.params.id - 1;
+    //condicion ? valor si verdadero : valor si es falso
+    (id >= 0 && id <= 150) ? res.status(200).send(pokemon[req.params.id - 1]) : res.status(404).send("Pokémon no encontrado");
+});
+
+//([A-Za-z]+) recibe peticiones que se encuzntren dentro de un abcdario
+app.get('/pokemon/:name([A-Za-z]+)', (req, res, next) => {
     const name = req.params.name;
-    for(i=0;i<pokemon.length;i++){
-        if(pokemon[i].name == name){
-            res.status(200);
-            res.send(pokemon[i])
+    /*
+    for(i = 0; i < pokemon.length; i++) {
+        if(pokemon[i].name.toUpperCase() == name.toUpperCase()){
+            res.status(200).send(pokemon[i])
         }
-    }
+    }*/
+    
+    //pk es un arreglo
+    const pk = pokemon.filter((p) => {
+
+        //condicion ? valor si verdadero : valor si es falso, toma dos valores verdaderos y regresa p
+      return (p.name.toUpperCase() == name.toUpperCase()) && p;
+        
+        
+    });
+    
+    
+    //condicion ? valor si verdadero : valor si es falso
+    (pk.length > 0) ? res.status(200).send(pk) : res.status(404).send("Pokémon no encontrado");
 
 });
 
