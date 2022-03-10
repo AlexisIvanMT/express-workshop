a181a59dc21af62c8ebe676802681854e8213eee
Alexis Ivan <alexis.ivan.mt@gmail.com>
2022-03-10 00:21:59 -0600

    Middleware para 404

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const bodyParser = require('body-parser');
+//const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const express = require('express');
 const app = express();
@@ -17,10 +17,14 @@ DELETE -Borrar un recursos
 */
 
 app.get("/", (req, res, next) => {
-   return res.status(200).send("Bienvenido al Pokedex"); 
+   return res.status(200).json({code: 1, message: "Bienvenido al Pokédex"}); 
 });
 
 app.use("/pokemon", pokemon);
+
+app.use((req, res, next) => {
+    return res.status(404).json({code: 404, message: "URL no encontrada"});
+});
 
 app.listen(process.env.PORT || 3000, ()=>{
     console.log("Server is runnig...")
--- routes/pokemon.js
+++ routes/pokemon.js
@@ -6,32 +6,32 @@ const db = require('../config/database')
 //quitamos la llaves no hay varible llamada pk en el json, al final agregamos .pokemon
 
 pokemon.post("/", (req, res, next) => {
-    return res.status(200).send(req.body);
+    return res.status(200).json(req.body);
 });
 
 //Para extraer solo los datos necesarios
-pokemon.get('/',async (req, res, next) => {
-    const pkmn =await db.query("SELECT * FROM pokemon");
-    
-    return res.status(200).json(pkmn);
+pokemon.get('/', async (req, res, next) => {
+    const pkmn = await db.query("SELECT * FROM pokemon"); 
+    return res.status(200).json({code: 1, message: pkmn});
 });
 
 //Para obtener un pokemon por id 
-pokemon.get('/:id([0-9]{1,3})',(req, res, next) => {
-    const id = req.params.id - 1;
+pokemon.get('/:id([0-9]{1,3})', async (req, res, next) => {
+    const id = req.params.id;
     //condicion ? valor si verdadero : valor si es falso
     
-    if (id >= 0 && id <= 150) {
+    if (id >= 1 && id <= 722) {
+       const pkmn = await db.query("SELECT * FROM pokemon WHERE pok_id=" + id +";");
+       return res.status(200).json({code:1, message: pkmn});
        
-       return res.status(200).send(pk[req.params.id - 1])
-       console.log(pk);
     }  
-       return res.status(404).send("Pokémon no encontrado"); 
+       return res.status(404).send({code:404, message:"Pokemon no encontrado"}); 
 });
 
 //([A-Za-z]+) recibe peticiones que se encuzntren dentro de un abcdario
-pokemon.get('/:name([A-Za-z]+)', (req, res, next) => {
+pokemon.get('/:name([A-Za-z]+)', async (req, res, next) => {
     const name = req.params.name;
+    const pkmn = await db.query("SELECT * FROM pokemon WHERE pok_name='"+ name + "';");
     /*
     for(i = 0; i < pokemon.length; i++) {
         if(pokemon[i].name.toUpperCase() == name.toUpperCase()){
@@ -40,17 +40,19 @@ pokemon.get('/:name([A-Za-z]+)', (req, res, next) => {
     }*/
     
     //pk es un arreglo
-    const pkmn = pk.filter((p) => {
-
+    //const pkmn = pk.filter((p) => {
+        
         //condicion ? valor si verdadero : valor si es falso, toma dos valores verdaderos y regresa p
-      return (p.name.toUpperCase() == name.toUpperCase()) && p;
+      //return (p.name.toUpperCase() == name.toUpperCase()) && p;
         
         
-    });
-    
+   // });
+    if(pkmn.length > 0) {
+        return res.status(200).json({code:1, message: pkmn});
+    }
     
     //condicion ? valor si verdadero : valor si es falso
-    (pkmn.length > 0) ? res.status(200).send(pkmn) : res.status(404).send("Pokémon no encontrado");
+    return res.status(404).send({code:404, message:"Pokemon no encontrado"}); 
 
 });
 
